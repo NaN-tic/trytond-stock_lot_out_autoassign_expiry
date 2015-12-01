024c572912e397961c1a74dd78b53ad089284303
resteve <resteve@zikzakmedia.com>
2015-12-01 11:24:36 +0100

    Tests

--- tests/test_stock_lot_out_autoassign_expiry.py
+++ tests/test_stock_lot_out_autoassign_expiry.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the stock_lot_out_autoassign_expiry module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockLotOutAutoassignExpiryTestCase(unittest.TestCase):
+class StockLotOutAutoassignExpiryTestCase(ModuleTestCase):
     'Test Stock Lot Out Autoassign Expiry module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'stock_lot_out_autoassign_expiry')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_lot_out_autoassign_expiry'
 
 
 def suite():
@@ -24,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockLotOutAutoassignExpiryTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
